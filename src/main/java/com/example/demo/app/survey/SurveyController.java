bd5642b4c9e75ddb6589c8c57be8fc1dbff981d2
mstbeat <mrbrightside82@ymail.com>
2020-04-17 17:56:10 +0900

    finish inquiry form and survey form

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,4 +1,4 @@
-package com.example.demo.app.inquiry;
+package com.example.demo.app.survey;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,45 +11,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/inquiry")
-public class InquiryController {
+@RequestMapping("/survey")
+public class SurveyController {
 
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, 
+	public String form(SurveyForm surveyForm,
 			Model model,
 			@ModelAttribute("complete") String complete) {
-		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+		model.addAttribute("title", "Survey Form");
+		return "survey/form";
 	}
 	
 	@PostMapping("/form")
-	public String formGoBack(InquiryForm inquiryForm, Model model) {
-		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+	public String formGoBack(SurveyForm surveyForm, Model model) {
+		model.addAttribute("title", "Survey Form");
+		return "survey/form";
 	}
 	
 	@PostMapping("/confirm")
-	public String confirm(@Validated InquiryForm inquiryForm, 
+	public String confirm(@Validated SurveyForm surveyForm, 
 			BindingResult result, 
 			Model model) {
 		if(result.hasErrors()) {
-			model.addAttribute("title", "Inquiry Form");
-			return "inquiry/form";
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
 		}
 		model.addAttribute("title", "Confirm Page");
-		return "inquiry/confirm";
+		return "survey/confirm";
 	}
 	
 	@PostMapping("/complete")
-	public String complete(@Validated InquiryForm inquiryForm,
+	public String complete(@Validated SurveyForm surveyForm,
 			BindingResult result,
 			Model model,
 			RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
-			model.addAttribute("title", "inquiryFrom");
-			return "inquiry/form";
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
 		}
-		redirectAttributes.addFlashAttribute("complete", "Registered!");
-		return "redirect:/inquiry/form";
+		redirectAttributes.addFlashAttribute("complete", "Thanks for your cooperation!");
+		return "redirect:/survey/form";
 	}
 }
